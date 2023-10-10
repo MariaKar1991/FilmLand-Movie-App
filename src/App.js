4c044e50469bbdc4977b76ef2be7c610d12bc218
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-10-11 00:48:04 +0300

    Update App.2.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=ac41c6b0";
+const API_URL = "https://www.omdbapi.com?apikey=ac41c6b0";
 
 const movie1 = {
   Title: "A Clockwork Orange",

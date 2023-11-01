a92d06bd953ed5d6ab7956be9a836da2661792ef
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 14:13:53 +0200

    Update index.2.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+// Get the root element by its id
 const root = document.getElementById("root");
+
+// Create a root for React to render into
 const rootElement = ReactDOM.createRoot(root);
+
+// Render the App component into the root element
 rootElement.render(<App />);
+
